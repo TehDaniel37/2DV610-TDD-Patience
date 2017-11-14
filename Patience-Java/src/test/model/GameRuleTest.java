37ffd3c3153b291dcaafb22277c69060c27e3616
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-11-14 12:32:22 +0100

    create empty classes and testclasses for the application

---
+++ Patience-Java/src/test/model/GameRuleTest.java
@@ -0,0 +1,13 @@
+package test.model;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameRuleTest {
+
+}
