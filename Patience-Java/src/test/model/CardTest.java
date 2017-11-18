8bcabe5482c28f12d3b957dd98887c5f00fc7372
Daniel Alm Grundström <daniel.alm.grundstrom@live.se>
2017-11-18 21:04:32 +0100

    RED: adds failing Card constructor test

--- Patience-Java/src/test/model/CardTest.java
+++ Patience-Java/src/test/model/CardTest.java
@@ -8,7 +8,20 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import main.Card;
+import main.Color;
+import main.Value;
+
 public class CardTest {
 
+    @Test
+    public void constructorShouldSetColorAndValue() {
+        Color expectedColor = Color.Hearts;
+        Value expectedValue = Value.Two;
 
+        Card card = new Card(expectedColor, expectedValue);
+
+        assertEquals(card.color, expectedColor);
+        assertEquals(card.value, expectedValue);
+    }
 }
