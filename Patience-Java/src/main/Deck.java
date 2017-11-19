20962812172a6f6d3458ff2d4a1946bb38b7ad63
Daniel Alm Grundström <daniel.alm.grundstrom@live.se>
2017-11-19 20:42:54 +0100

    GREEN: Constructor of Deck now populates itself with 52 unique cards

--- Patience-Java/src/main/Deck.java
+++ Patience-Java/src/main/Deck.java
@@ -1,5 +1,18 @@
 package main;
 
+import java.util.ArrayList;
 
 public class Deck {
+
+    public ArrayList<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<Card>();
+
+        for (Color color : Color.values()) {
+            for (Value value : Value.values()) {
+                cards.add(new Card(color, value));
+            }
+        }
+    }
 }
