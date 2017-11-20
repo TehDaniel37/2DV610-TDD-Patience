7f74988f584e87a1e492394e868665a9785584be
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-11-20 11:23:56 +0100

    REFACTOR: imports clean up, create @Before method and add sut as class variable

--- Patience-Java/src/test/model/StackTest.java
+++ Patience-Java/src/test/model/StackTest.java
@@ -4,56 +4,55 @@ import main.model.Card;
 import main.model.Color;
 import main.model.Stack;
 import main.model.Value;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
-
-import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class StackTest {
 
-    int expectedSize;
+    private int expectedSize;
+    private static Stack sut;
+
+    @Before
+    public static void beforeEach() {
+        sut = new Stack();
+    }
 
 
     @Test
     public void stackConstructorShouldCreateEmptyCardArrayList() {
-        Stack stack = new Stack();
+        Stack sut = new Stack();
         expectedSize = 0;
-        ArrayList<Card> stackOfCards = stack.getStack();
 
-        assertEquals(stackOfCards.size(), expectedSize);
+        assertEquals(sut.getStack().size(), expectedSize);
     }
 
     @Test
     public void addCardShouldIncreaseSizeOfStackByOne() {
         expectedSize = 1;
         Card card = mock(Card.class);
-        Stack stack = new Stack();
+        sut = new Stack();
 
-        stack.addCard(card);
+        sut.addCard(card);
 
 
-        assertEquals(stack.getStack().size(), expectedSize);
+        assertEquals(sut.getStack().size(), expectedSize);
     }
 
     @Test
     public void getTopShouldReturnLatestCardAdded() {
         Card card = new Card(Color.Spades, Value.Ace);
-        Stack stack = new Stack();
-        stack.addCard(card);
+        sut = new Stack();
+        sut.addCard(card);
 
-        card = stack.getTop();
+        card = sut.getTop();
 
         assertEquals(card.getColor(), Color.Spades);
         assertEquals(card.getValue(), Value.Ace);
 
     }
+
+
 }
