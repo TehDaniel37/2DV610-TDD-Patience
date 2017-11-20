242a5ebc824ed8809457d47a85efe78acecf9bdf
Daniel Alm Grundström <daniel.alm.grundstrom@live.se>
2017-11-20 14:44:18 +0100

    RED: Adds failing test for deck shuffle

--- Patience-Java/src/test/model/DeckTest.java
+++ Patience-Java/src/test/model/DeckTest.java
@@ -3,11 +3,16 @@ package test.model;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Collections;
+import java.util.Random;
 
 import main.model.Deck;
+import main.model.Color;
+import main.model.Value;
 import main.model.Card;
 
 public class DeckTest {
@@ -23,5 +28,38 @@ public class DeckTest {
         
         assertEquals(actualCards.size(), expectedSize);
         assertEquals(setOfCards.size(), expectedSize);
+    }
+    
+    @Test
+    public void shuffleShouldRandomizeCardOrder() {
+        final int seed = 123456789;
+        Random random = new Random(seed);
+        
+        ArrayList<Card> expectedUnshuffled = new ArrayList<>();
+        ArrayList<Card> expectedShuffled = new ArrayList<>();
+
+        for (Color color : Color.values()) {
+            for (Value value : Value.values()) {
+                Card mockCard1 = mock(Card.class);
+                Card mockCard2 = mock(Card.class);
+                
+                when(mockCard1.getColor()).thenReturn(color);
+                when(mockCard1.getValue()).thenReturn(value);
+                when(mockCard2.getColor()).thenReturn(color);
+                when(mockCard2.getValue()).thenReturn(value);
+                
+                expectedUnshuffled.add(mockCard1);
+                expectedShuffled.add(mockCard2);
+            }
+        }
+        
+        Collections.shuffle(expectedShuffled, random);
+        
+        Deck sut = new Deck();
+        sut.setCards(expectedUnshuffled);
+        sut.shuffle(seed);
+        ArrayList<Card> actualShuffled = sut.getCards();
+        
+        assertEquals(actualShuffled, expectedShuffled);
     }
 }
