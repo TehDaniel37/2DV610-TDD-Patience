23de20db2ab660812a40e398530f63eff555a919
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-11-20 10:23:10 +0100

    RED: Stack constructor test should create an array Fail

--- Patience-Java/src/main/model/Stack.java
+++ Patience-Java/src/main/model/Stack.java
@@ -1,5 +1,19 @@
 package main.model;
 
 
+import java.util.ArrayList;
+
 public class Stack {
+
+    private ArrayList<Card> stack;
+
+    public Stack() {
+
+    }
+
+    public ArrayList<Card> getStack() {
+        return stack;
+    }
+
+
 }
--- Patience-Java/src/test/model/StackTest.java
+++ Patience-Java/src/test/model/StackTest.java
@@ -1,13 +1,26 @@
 package test.model;
 
+import main.model.Card;
+import main.model.Stack;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class StackTest {
 
+
+    @Test
+    public void stackConstructorShouldCreateEmptyCardArrayList() {
+        Stack stack = new Stack();
+        int expectedSize = 0;
+        ArrayList<Card> stackOfCards = stack.getStack();
+
+        assertEquals(stackOfCards.size(), expectedSize);
+    }
 }
