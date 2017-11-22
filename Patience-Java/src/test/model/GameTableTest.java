01e105ebf2770a97e63c796680fcde3ad134b704
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-11-22 10:19:07 +0100

    RED: mergeStack: top stack should be empty after merge. test Failed (also added successfull test - top card should be top card of top stack after merge)

--- Patience-Java/src/test/model/GameTableTest.java
+++ Patience-Java/src/test/model/GameTableTest.java
@@ -1,6 +1,8 @@
 package test.model;
 
+import main.exception.EmptyCardStackException;
 import main.model.Card;
+import main.model.Color;
 import main.model.GameTable;
 import main.model.Stack;
 import org.junit.AfterClass;
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GameTableTest {
 
@@ -63,6 +66,52 @@ public class GameTableTest {
 
         assertEquals(expectedSize, stackBottom.getStack().size());
     }
+
+    @Test
+    public void topCardAfterMergeShouldBeLatestAddedInTopStack() throws EmptyCardStackException {
+        Stack stackBottom = new Stack();
+        Stack stackTop = new Stack();
+
+        Card card = mock(Card.class);
+        when(card.getColor()).thenReturn(Color.Hearts);
+        stackBottom.addCard(card);
+        card = mock(Card.class);
+        when(card.getColor()).thenReturn(Color.Spades);
+        stackBottom.addCard(card);
+        card = mock(Card.class);
+        when(card.getColor()).thenReturn(Color.Diamonds);
+        stackTop.addCard(card);
+        card = mock(Card.class);
+        when(card.getColor()).thenReturn(Color.Clubs);
+        stackTop.addCard(card);
+
+        Color expectedColor = Color.Clubs;
+
+        sut.mergeStacks(stackBottom, stackTop);
+
+        assertEquals(expectedColor, stackBottom.getTop().getColor());
+    }
+
+    @Test
+    public void topStackShouldBeEmptyAfterMerge() {
+        Stack stackBottom = new Stack();
+        Stack stackTop = new Stack();
+
+        Card card = mock(Card.class);
+
+        stackBottom.addCard(card);
+        stackBottom.addCard(card);
+        stackTop.addCard(card);
+        stackTop.addCard(card);
+
+        expectedSize = 0;
+
+        sut.mergeStacks(stackBottom, stackTop);
+
+        assertEquals(expectedSize, stackTop.getStack().size());
+    }
+
+
 
 
 }
