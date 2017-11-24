d969751eacddcf999720024bd44cd24ed20888b3
Daniel Alm <daniel.alm.grundstrom@protonmail.com>
2017-11-24 03:14:10 +0100

    RED: Adds failing test for checking if two stacks are mergeable

--- Patience-Java/src/test/model/GameTest.java
+++ Patience-Java/src/test/model/GameTest.java
@@ -1,15 +1,17 @@
 package test.model;
 
+import main.exception.EmptyCardStackException;
+import main.model.*;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import sun.invoke.empty.Empty;
 
 import static org.junit.Assert.*;
-
-import main.model.Game;
-import main.model.Stack;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GameTest {
     
@@ -34,5 +36,29 @@ public class GameTest {
     @Test
     public void constructorShouldCreateGameTable() {
         assertNotNull(sut.getGameTable());
+    }
+
+    @Test
+    public void stacksMergeableShouldReturnTrueWhenCardsHaveSameColor() throws EmptyCardStackException {
+        final int bottomStackPos = 0;
+        final int topStackPos = 1;
+
+        Card mockCardBottom = mock(Card.class);
+        Card mockCardTop = mock(Card.class);
+
+        when(mockCardBottom.getColor()).thenReturn(Color.Hearts);
+        when(mockCardTop.getColor()).thenReturn(Color.Hearts);
+        when(mockCardBottom.getValue()).thenReturn(Value.Two);
+        when(mockCardTop.getValue()).thenReturn(Value.Three);
+
+        Stack mockStackBottom = mock(Stack.class);
+        Stack mockStackTop = mock(Stack.class);
+
+        when(mockStackBottom.getTop()).thenReturn(mockCardBottom);
+        when(mockStackTop.getTop()).thenReturn(mockCardTop);
+        when(mockStackTop.getPosition()).thenReturn(bottomStackPos);
+        when(mockStackBottom.getPosition()).thenReturn(topStackPos);
+
+        assertTrue(sut.stacksMergable(mockStackBottom, mockStackTop));
     }
 }
