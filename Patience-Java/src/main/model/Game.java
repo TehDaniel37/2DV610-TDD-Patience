a33fe77e3d9fb2ef6d92c0caa53292f6510030b5
Daniel Alm <daniel.alm.grundstrom@protonmail.com>
2017-11-25 01:28:40 +0100

    REFACTOR: Implements stacksMergeable properly

--- Patience-Java/src/main/model/Game.java
+++ Patience-Java/src/main/model/Game.java
@@ -1,5 +1,7 @@
 package main.model;
 
+import main.exception.EmptyCardStackException;
+
 import java.util.ArrayList;
 
 public class Game {
@@ -24,6 +26,22 @@ public class Game {
     }
 
     public boolean stacksMergeable(Stack stack1, Stack stack2) {
-        return true;
+        int positionDifference = Math.abs(stack2.getPosition() - stack1.getPosition());
+
+        if (!(positionDifference == 1 || positionDifference == 3)) {
+            return false;
+        }
+
+        Card card1;
+        Card card2;
+
+        try {
+            card1 = stack1.getTop();
+            card2 = stack2.getTop();
+        } catch (EmptyCardStackException ex) {
+            return false;
+        }
+
+        return card1.getColor() == card2.getColor() || card1.getValue() == card2.getValue();
     }
 }
