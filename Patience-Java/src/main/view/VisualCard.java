aa74f75af3c8d4f67abd82094164441884d7be1d
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-11-27 14:22:26 +0100

    Add empty classes for view, and update class diagram wirh new classes

---
+++ Patience-Java/src/main/view/VisualCard.java
@@ -0,0 +1,7 @@
+package main.view;
+
+/**
+ * Created by Caroline Nilsson on 2017-11-27.
+ */
+public class VisualCard {
+}
---
+++ Patience-Java/src/test/view/GameTableViewTest.java
@@ -0,0 +1,13 @@
+package test.view;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTableViewTest {
+
+}
