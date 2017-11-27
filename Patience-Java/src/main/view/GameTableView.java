bff613afa9c7de14da6c8df8cc98499868f74d63
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-11-27 17:47:50 +0100

    Updated Class diagram

--- Patience-Java/src/main/view/GameTableView.java
+++ Patience-Java/src/main/view/GameTableView.java
@@ -1,7 +1,16 @@
 package main.view;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Created by Caroline Nilsson on 2017-11-27.
  */
-public class GameTableView implements GameObserver {
+public class GameTableView extends Application implements GameObserver {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
