4ef23a073961eb212d61920e4632fe7e96a05025
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2017-12-05 19:06:56 +0100

    RED: Adds failing test for VisualStack constructor

--- Patience-Java/src/main/view/VisualStack.java
+++ Patience-Java/src/main/view/VisualStack.java
@@ -1,7 +1,15 @@
 package main.view;
 
+import main.model.Stack;
+
 /**
  * Created by Caroline Nilsson on 2017-11-27.
  */
 public class VisualStack {
+
+    public VisualCard topCard;
+
+    public VisualStack(Stack stack) {
+
+    }
 }
--- Patience-Java/src/test/view/VisualStackTest.java
+++ Patience-Java/src/test/view/VisualStackTest.java
@@ -1,5 +1,11 @@
 package test.view;
 
+import main.exception.EmptyCardStackException;
+import main.model.Card;
+import main.model.Color;
+import main.model.Stack;
+import main.model.Value;
+import main.view.VisualStack;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.After;
@@ -7,7 +13,25 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class VisualStackTest {
 
+    @Test
+    public void constructorShouldCreateVisualCard() {
+        Stack mockStack = mock(Stack.class);
+        Card mockCard = mock(Card.class);
+
+        when(mockCard.getColor()).thenReturn(Color.Spades);
+        when(mockCard.getValue()).thenReturn(Value.Six);
+
+        try {
+            when(mockStack.getTop()).thenReturn(mockCard);
+        } catch (EmptyCardStackException ignored) { }
+
+        VisualStack sut = new VisualStack(mockStack);
+
+        assertNotNull(sut.topCard);
+    }
 }
